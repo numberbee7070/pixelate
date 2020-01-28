08a66955dc16d2664d475413c7663b19daf508b2
yashking007 <yashgarg2910@gmail.com>
2020-01-28 20:51:36 +0530

    Optimized for Red colour

--- pixelate_color.py
+++ pixelate_color.py
@@ -25,7 +25,21 @@ def getRange(img):
 
     return (np.array([h_min,s_min,v_min],dtype='uint8'),np.array([h_max,s_max,v_max],dtype='uint8'))
 
+def getMask(img, rng):
+    if rng[0][0] < rng[1][0]:
+        return cv2.inRange(img, rng[0], rng[1])
+    else:
+        upr1 = np.array([180,rng[1][1], rng[1][2]], dtype='uint8')
+        lwr1 = np.array([0,rng[0][1], rng[0][2]], dtype='uint8')
+        print(rng[0], upr1)
+        print(lwr1, rng[1])
+        mask1 = cv2.inRange(img, rng[0], upr1)
+        mask2 = cv2.inRange(img, lwr1, rng[1])
+        return mask1+mask2
+
+
 img = cv2.imread('pixelate0.jpeg')
+print(img.shape)
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 cv2.imshow('image', img)
 
@@ -33,5 +47,5 @@ rect = cv2.selectROI('image', img)
 cropped_hsv = img_hsv[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]]
 rng = getRange(cropped_hsv)
 print(rng)
-mask = cv2.inRange(img_hsv, rng[0], rng[1])
+mask = getMask(img_hsv, rng)
 cv2.imshow('mask', mask)
